69514753d5d5431471b427c6d7a3dd304f59caa6
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-10-30 13:22:46 +0300

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Павел Афанасьев on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
